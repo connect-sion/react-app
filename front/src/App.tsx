ea17582a4bc18c48d9fb87bc6f1cb150346e3509
maasencioh <maasencioh@gmail.com>
2020-05-08 08:22:19 -0500

    Adds environmental variables for front

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 
 import VideoPlayer from "./VideoPlayer";
 
+const host = process.env.REACT_APP_HOST || "localhost";
+const port = process.env.REACT_APP_PORT || "8080";
+const stream = process.env.REACT_APP_STREAM || "test";
+
 const App = () => {
   const [error, setError] = useState(false);
   const videoJsOptions = {
@@ -9,7 +13,7 @@ const App = () => {
     controls: true,
     sources: [
       {
-        src: "http://localhost:8080/hls/test.m3u8",
+        src: `http://${host}:${port}/live/${stream}.m3u8`,
         type: "application/x-mpegURL",
       },
     ],
