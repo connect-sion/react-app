22010a952d6002be66abca01f19607b0a5adce03
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-06-12 07:45:01 -0500

    implementation using simple peer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,60 +1,49 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef } from 'react';
 import io from 'socket.io-client';
+import Peer from 'simple-peer';
 import Layout from './Layout';
 
 const host = process.env.REACT_APP_HOST || 'localhost';
 const port = process.env.REACT_APP_PORT || '8080';
 
+interface Signal {
+  peerId: string;
+  signal: any;
+}
+
 const App = () => {
   const audio = useRef<HTMLAudioElement>(null);
   const socket = io(`http://${host}:${port}`);
-  const config = { iceServers: [{ urls: ['stun:stun.l.google.com:19302'] }] };
 
-  useEffect(() => {
-    let peerConnection: RTCPeerConnection;
-    socket.on('offer', (id: string, description: RTCSessionDescriptionInit) => {
-      peerConnection = new RTCPeerConnection(config);
-      peerConnection
-        .setRemoteDescription(description)
-        .then(() => peerConnection.createAnswer())
-        .then((sdp) => peerConnection.setLocalDescription(sdp))
-        .then(() => {
-          socket.emit('answer', id, peerConnection.localDescription);
-        })
-        .catch(console.error);
-      peerConnection.ontrack = (event) => {
-        console.log('Incoming stream');
-        if (audio?.current) audio.current.srcObject = event.streams[0];
-      };
-      peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-          socket.emit('candidate', id, event.candidate);
-        }
-      };
-    });
+  let peer = new Peer({ initiator: true });
+  socket.on('connect', () => {
+    console.log(socket.id);
+    socket.emit('peer', { peerId: socket.id });
+  });
 
-    socket.on('candidate', (id: string, candidate: RTCIceCandidateInit) => {
-      if (peerConnection.remoteDescription) {
-        peerConnection
-          .addIceCandidate(new RTCIceCandidate(candidate))
-          .catch((e) => console.error(e));
-      }
-    });
+  peer.on('connect', () => console.log('connected'));
 
-    socket.on('connect', () => {
-      socket.emit('watcher');
-    });
+  // peer signaling
+  socket.on('signal', (data: Signal) => {
+    if (data.peerId === socket.id) {
+      peer.signal(data.signal);
+    }
+  });
+  peer.on('signal', (signal) => {
+    socket.emit('signal', { signal, peerId: socket.id });
+  });
 
-    socket.on('broadcaster', () => {
-      socket.emit('watcher');
-    });
+  peer.on('error', (error) => {
+    console.error(error);
+  });
 
-    socket.on('disconnectPeer', () => {
-      peerConnection.close();
-    });
-    return () => {
-      socket.close();
-    };
+  peer.on('stream', (stream) => {
+    console.log(stream);
+
+    if (audio.current) {
+      audio.current.srcObject = stream;
+      audio.current.play();
+    }
   });
 
   return (
